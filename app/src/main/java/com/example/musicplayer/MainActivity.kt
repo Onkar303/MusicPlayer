18752f327a053efedc4d604bf306a9a88941005c
onkar <onkarkalpavriksha>
2019-12-14 00:08:17 +1100

    First actual commit

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"this is sparta",Toast.LENGTH_SHORT).show()
     }
 }
